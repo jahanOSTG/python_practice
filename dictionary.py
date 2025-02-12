ea03962cb29c45a5cc64e137eb7a85267f33249b
Faria Jahan Janie <janiefaria315@gmail.com>
2025-02-13 01:02:43 +0600

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+# print information of 552 and if  print
+studentID={
+    550 : "Masudul karim",
+    551 : "Faria jahan janie",
+    557 : "Richmond Antor"
+    
+}
+print(studentID.get(552,"Invalid Key"))
---
+++ tuples.py
@@ -0,0 +1,7 @@
+studentID=(
+    ("Faria Jahan Janie",552,27),
+    "Riochmond Antor Biswas",
+    "Hossain Khan"
+    
+)
+print(studentID[0:])
