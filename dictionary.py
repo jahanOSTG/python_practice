3718365bf16f8ee64d7c5a37f6a4ce5d4b2ea067
Faria Jahan Janie <janiefaria315@gmail.com>
2025-02-13 12:24:13 +0600

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,8 +1,8 @@
 # print information of 552 and if  print
 studentID={
-    550 : "Masudul karim",
+   
     551 : "Faria jahan janie",
     557 : "Richmond Antor"
     
 }
-print(studentID.get(552,"Invalid Key"))
+print(studentID.get(557,"Invalid Key"))
